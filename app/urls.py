1ec76c35a5cf7acb2a1c367b72af4559a2733369
Matthew Joyce <matsjoyce@gmail.com>
2018-02-07 21:12:52 +0000

    Add run and submit pages

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,15 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.leaderboard, name="app_leaderboard"),
-    url(r"^scripts/view/(?P<script_id>\d+)/$", views.scripts_view, name="app_scripts_view"),
-    url(r"^scripts/new/$", views.scripts_new, name="app_scripts_new"),
-]
 
+    url(r"^scripts/(?P<script_id>\d+)/view/$", views.scripts_view, name="app_scripts_view"),
+
+    url(r"^scripts/new/$", views.scripts_new, name="app_scripts_new"),
+    url(r"^scripts/(?P<parent_id>\d+)/fork/$", views.scripts_new, name="app_scripts_fork"),
+
+    url(r"^scripts/run/$", views.scripts_run, name="app_scripts_run"),
+    url(r"^scripts/(?P<script_id>\d+)/run/$", views.scripts_run, name="app_scripts_run_existing"),
+
+    url(r"^scripts/submit/$", views.scripts_submit, name="app_scripts_submit"),
+    url(r"^scripts/(?P<script_id>\d+)/submit/$", views.scripts_submit, name="app_scripts_submit_existing"),
+]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseBadRequest
+import json
 from . import models
 
 
@@ -18,5 +20,100 @@ def scripts_view(request, *, script_id):
         })
 
 
-def scripts_new(request):
-    return render(request, "pages/scripts/new.html")
+def scripts_new(request, *, parent_id=None):
+    # Default is a brand new script
+    name = ""
+    code = ""
+    parent = None
+
+    # Already editing, extract data from POST
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        code = request.POST.get("code", "")
+        if request.POST.get("parent"):
+            parent = get_object_or_404(models.Script, id=request.POST["parent"])
+
+    # Forking another script
+    elif parent_id:
+        parent = get_object_or_404(models.Script, id=parent_id)
+        name = f"Fork of {parent.name}"
+        code = parent.code
+
+    return render(request, "pages/scripts/new.html", {
+            "name": name,
+            "code": code,
+            "parent": parent
+        })
+
+
+def scripts_run(request, *, script_id=None):
+    # Run of a new script
+    if request.method == "POST":
+        if (not request.POST.get("name")
+            or not request.POST.get("code")):
+            return HttpResponseBadRequest()
+
+        script = None
+        name = request.POST["name"]
+        code = request.POST["code"]
+        parent = request.POST["parent"]
+
+    # Run of an existing script
+    elif script_id:
+        script = get_object_or_404(models.Script, id=script_id)
+        name = script.name
+        code = script.code
+        parent = None
+
+    else:
+        return HttpResponseBadRequest()
+
+    return render(request, "pages/scripts/run.html", {
+            "script": script,
+            "name": name,
+            "code": code,
+            "parent": parent
+        })
+
+
+def scripts_submit(request, *, script_id=None):
+    if (request.method != "POST"
+        or not request.POST.get("scores")
+        or not request.POST.get("track")):
+        return HttpResponseBadRequest()
+
+    # Additional score of an existing script
+    elif script_id:
+        script = get_object_or_404(models.Script, id=script_id)
+
+    # Submission of a new script
+    else:
+        if (not request.POST.get("name")
+            or not request.POST.get("code")):
+            return HttpResponseBadRequest()
+
+        name = request.POST["name"]
+        code = request.POST["code"]
+        if request.POST.get("parent"):
+            parent = get_object_or_404(models.Script, id=request.POST["parent"])
+        else:
+            parent = None
+        script = models.Script(name=name, code=code, parent=parent)
+        script.save()
+
+    scores = request.POST["scores"]
+    track = get_object_or_404(models.Track, id=request.POST["track"])
+
+    try:
+        jsscores = json.loads(scores)
+        scores = {"time_taken": int(jsscores["time_taken"])}
+    except Exception:
+        return HttpResponseBadRequest()
+
+    score = models.Score(script=script, track=track, scores=scores)
+    score.save()
+
+    return render(request, "pages/scripts/submit.html", {
+            "script": script,
+            "score": score
+        })
