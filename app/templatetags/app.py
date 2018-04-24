992e8a895e940802e666e8761daa156ce67422a0
Matthew Joyce <matsjoyce@gmail.com>
2018-04-24 11:53:01 +0100

    Round values to 2dp

--- app/templatetags/app.py
+++ app/templatetags/app.py
@@ -10,3 +10,20 @@ register = template.Library()
 @register.filter
 def load_docs(value):
     return (DOCS_DIR / value).read_text()
+
+
+@register.filter
+def round2dp(value):
+    try:
+        return round(value, 2)
+    except:
+        return value
+
+
+@register.filter
+def mapround(value):
+    if isinstance(value, dict):
+        return {k:round2dp(v) for k, v in value.items()}
+    elif isinstance(value, list):
+        return list(map(round2dp, value))
+    return value
